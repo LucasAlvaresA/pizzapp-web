7acfa72eb94f69b323749cc07434c84fd747d7c7
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-01-26 09:18:30 -0300

    ✨ create basic structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Pizzapp - Login</title>
+            </Head>
+            <div>
+                <h1>Pizzapp</h1>
+            </div>
+        </>
+    );
+}
