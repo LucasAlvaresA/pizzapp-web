9b72ed58935c72c84ac887de59ac693d151d4763
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-02-05 17:03:25 -0300

    ✨ create signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,16 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../../styles/home.module.scss";
-import logoImg from "../../public/logo.png";
-import { Input } from "../components/ui/Input";
-import { Button } from "../components/ui/Button";
+import styles from "../../../styles/home.module.scss";
+import logoImg from "../../../public/logo.png";
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
     return (
         <>
             <Head>
-                <title>Pizzapp - Login</title>
+                <title>Pizzapp - Sign up</title>
             </Head>
             <div className={styles.containerCenter}>
                 <Image
@@ -19,18 +19,19 @@ export default function Home() {
                     className={styles.logo}
                 />
                 <div className={styles.login}>
-                    <h1>Login</h1>
+                    <h1>Sign Up</h1>
                     <form>
+                        <Input placeholder="Your name" type="text" />
                         <Input placeholder="E-mail" type="text" />
                         <Input placeholder="Password" type="password" />
                         <Button type="submit" loading={false}>
-                            Login
+                            Sign up
                         </Button>
                     </form>
 
-                    <Link href="/signup">
+                    <Link href="/">
                         <div className={styles.text}>
-                            Don't have an account?
+                            Already have an account?
                         </div>
                     </Link>
                 </div>
