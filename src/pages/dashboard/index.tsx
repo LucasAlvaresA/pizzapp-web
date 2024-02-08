2ff9dbd08d515fbaab01255faab2324f9a1a315f
Lucas Alvares <lucas.alvaress@hotmail.com>
2024-02-08 16:15:55 -0300

    ✨ create initial template for dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Header } from "../../components/Header";
+import styles from "./styles.module.scss";
+import { FiRefreshCcw } from "react-icons/fi";
 
 export default function Dashboard() {
     return (
@@ -10,7 +12,23 @@ export default function Dashboard() {
             </Head>
             <div>
                 <Header />
-                <h1> dashboard</h1>
+                <main className={styles.container}>
+                    <div className={styles.header}>
+                        <h1> Last orders</h1>
+                        <button>
+                            <FiRefreshCcw color="#3fffa3" size={25} />
+                        </button>
+                    </div>
+
+                    <article className={styles.listOrder}>
+                        <section className={styles.orderItem}>
+                            <button>
+                                <div className={styles.tag}></div>
+                                <span>Table 30</span>
+                            </button>
+                        </section>
+                    </article>
+                </main>
             </div>
         </>
     );
